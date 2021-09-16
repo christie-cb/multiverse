1418918209072b147466297a3933fd391cedff3a
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-16 13:25:16 +0100

    book class finished

--- classes/book.js
+++ classes/book.js
@@ -4,4 +4,12 @@ class Book {
         this.author = author;
         this.latestEdition = latestEdition;
     }
+    newEdition() {
+        this.latestEdition += 1;
+    }
 }
+//
+//const hamlet = new Book({title: "Hamlet", author: "Shakey", latestEdition: 1});
+//console.log(hamlet);
+//hamlet.newEdition();
+//console.log(hamlet);
