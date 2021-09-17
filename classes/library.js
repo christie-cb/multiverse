0841d031ff1eabd981d4c5a9658324f6e8b52b88
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-17 09:23:40 +0100

    Prettier formatting & got rid of latestEdition

--- classes/library.js
+++ classes/library.js
@@ -2,29 +2,25 @@ const Book = require("./book");
 const Author = require("./author");
 
 const shakey = new Author({ name: "shakey", yearOfBirth: "1564" });
-const hamlet = new Book({ title: "Hamlet", author: shakey, latestEdition: 1 });
-console.log(hamlet);
-hamlet.newEdition();
-console.log(hamlet);
-
+const hamlet = new Book({ title: "Hamlet", author: shakey });
 const tempest = new Book({
-  title: "The Tempest",
-  author: shakey,
-  latestEdition: 1,
+    title: "The Tempest",
+    author: shakey,
 });
 
-console.log(tempest);
-
 const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
 
 readline.question("Who are you?", (name) => {
-  console.log(`Hey there ${name}!`);
-  readline.question("What do you wanna read?", (name) => {
-    let book = new Book({ title: name, author: undefined, latestEdition: 1 });
-    console.log(book);
-    readline.close();
-  });
+    console.log(`Hey there ${name}!`);
+    readline.question("What do you wanna read?", (name) => {
+        let book = new Book({
+            title: name,
+            author: undefined,
+        });
+        console.log(book);
+        readline.close();
+    });
 });
