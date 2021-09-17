629b552b74d21f3c235b9983f1a95082e84b304e
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-17 10:12:04 +0100

    Added passing tests for books

---
+++ classes/book.test.js
@@ -0,0 +1,29 @@
+const Book = require("./book");
+const Author = require("./author");
+
+test("books should have an author", () => {
+    const testAuthor = new Author({ name: "name", yearOfBirth: "2021" });
+    const book = new Book({ title: "book", author: testAuthor });
+    expect(book.author).toBe(testAuthor);
+});
+
+test("books should have a title", () => {
+    const testAuthor = new Author({ name: "name", yearOfBirth: "2021" });
+    const book = new Book({ title: "book", author: testAuthor });
+    expect(book.title).toBe("book");
+});
+
+test("books should have a latestEdition", () => {
+    const testAuthor = new Author({ name: "name", yearOfBirth: "2021" });
+    const book = new Book({ title: "book", author: testAuthor });
+    expect(book._latestEdition).toBe(1);
+});
+
+test("books should have a newEdition to incrememnt latestEdition", () => {
+    const testAuthor = new Author({ name: "name", yearOfBirth: "2021" });
+    const book = new Book({ title: "book", author: testAuthor });
+    expect(book._latestEdition).toBe(1);
+    book.newEdition();
+    expect(book._latestEdition).toBe(2);
+});
+
