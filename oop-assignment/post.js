b6e7d778c84d2062f100c37400bd369d0c5381dd
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-20 11:25:11 +0100

    tests passing for post

--- oop-assignment/post.js
+++ oop-assignment/post.js
@@ -1,3 +1,6 @@
+const { v4: uuidv4 } = require("uuid");
+const Comment = require("./comment");
+
 class Post {
     constructor({ title, date, author, text, pageId, postId }) {
         this.title = title;
@@ -6,6 +9,16 @@ class Post {
         this.text = text;
         this.pageId = pageId;
         this.postId = postId;
+    }
+
+    addComment({ text, author }) {
+        return new Comment({
+            text: text,
+            date: Date(Date.now()),
+            author: author,
+            postId: this.postId,
+            commentId: uuidv4(),
+        });
     }
 }
 
