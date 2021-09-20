9f5b04e48799fd20ea7bc12a7e4271698dc39628
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-20 13:49:16 +0100

    Updated test for refactor to make for createPost

--- oop-assignment/tests/user.test.js
+++ oop-assignment/tests/user.test.js
@@ -13,7 +13,7 @@ test("User includes username and user id", () => {
     const username = "username";
     const testId = "_user";
     const user = new User({ username: username, userId: testId });
-    const post = user.createPost();
+    const post = user.createPost({ title: "title", text: "txt", pageId: "id" });
     expect(post).toBeInstanceOf(Post);
     expect(post.author).toBe(user);
 });
