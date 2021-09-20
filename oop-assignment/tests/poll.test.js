22b1430322982d505a041e9535336b58d5d145f2
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-20 13:43:38 +0100

    Updated poll test to ensure it is similar to a post.

--- oop-assignment/tests/poll.test.js
+++ oop-assignment/tests/poll.test.js
@@ -1,4 +1,31 @@
 const { Poll, Options } = require("../poll");
+const Post = require("../post");
+const User = require("../user");
+
+test("poll inherits from post", () => {
+    const params = {
+        title: "title",
+        author: new User({ username: "user", userId: "_user" }),
+        date: Date(Date.now()),
+        text: "hello world",
+        postId: "_post",
+        pageId: "_page",
+    };
+    const options = new Options({ A: "scone", B: "definitely scone" });
+    const post = new Poll({
+        question: "Scone or scone?",
+        options: options,
+        title: params.title,
+        author: params.author,
+        date: params.date,
+        text: params.text,
+        postId: params.postId,
+        pageId: params.pageId,
+    });
+    expect(poll.author).toBe(params.author);
+    expect(poll).toBeInstanceOf(Post);
+});
+
 test("poll includes options", () => {
     const options = new Options({ A: "scone", B: "definitely scone" });
     const poll = new Poll({ question: "Scone or scone?", options: options });
