a2dbf76341cbbd10235e951b6f95f18bb6f408da
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-20 10:19:04 +0100

    Fixed syntax mistake on arguments for class

--- oop-assignment/tests/forum-page.test.js
+++ oop-assignment/tests/forum-page.test.js
@@ -3,7 +3,7 @@ const ForumPage = require("../forum-page");
 test("forum page contains title and page id", () => {
     const titleString = "test title";
     const idString = "_test";
-    const forumPage = new ForumPage(title: titleString, pageId: idString);
+    const forumPage = new ForumPage({ title: titleString, pageId: idString });
     expect(forumPage.title).toBe(titleString);
     expect(forumPage.pageId).toBe(idString);
 });
