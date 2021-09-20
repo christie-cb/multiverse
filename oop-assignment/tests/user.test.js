492d6ed154fa1f854fb84e8ff0266bc3509baf1f
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-20 10:21:55 +0100

    Added test for createPost

--- oop-assignment/tests/user.test.js
+++ oop-assignment/tests/user.test.js
@@ -1,4 +1,5 @@
 const User = require("../user");
+const Post = require("../post");
 
 test("User includes username and user id", () => {
     const username = "username";
@@ -6,4 +7,12 @@ test("User includes username and user id", () => {
     const user = new User({ username: username, userId: testId });
     expect(user.username).toBe(username);
     expect(user.userId).toBe(testId);
+});
+
+test("User includes username and user id", () => {
+    const username = "username";
+    const testId = "_user";
+    const user = new User({ username: username, userId: testId });
+    const post = user.createPost();
+    expect(post).toBeInstanceOf(Post);
 });
