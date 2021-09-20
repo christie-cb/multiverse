0018c300473a02df8314632cc673520534ecdbd3
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-20 10:19:17 +0100

    Added first test for user.

---
+++ oop-assignment/tests/user.test.js
@@ -0,0 +1,9 @@
+const User = require("../user");
+
+test("User includes username and user id", () => {
+    const username = "username";
+    const testId = "_user";
+    const user = User({ username: username, userId: testId });
+    expect(user.username).toBe(username);
+    expect(user.userId).toBe(testId);
+});
