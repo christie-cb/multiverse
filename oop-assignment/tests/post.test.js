177240f52f100ea12bf02af18227129f988111ee
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-20 10:59:48 +0100

    Added test for adding comment to post>

--- oop-assignment/tests/post.test.js
+++ oop-assignment/tests/post.test.js
@@ -1,5 +1,6 @@
 const Post = require("../post");
 const User = require("../user");
+const Comment = require("../comment");
 
 test("post has title, author, date, text", () => {
     const params = {
@@ -21,4 +22,18 @@ test("post has title, author, date, text", () => {
     Object.keys(params).forEach((key) => {
         expect(post[key]).toBe(params[key]);
     });
+});
+
+test("can add comments to post", () => {
+    const post = new Post({
+        title: "title",
+        author: new User({ username: "user", userId: "_user" }),
+        date: Date(Date.now()),
+        text: "hello world",
+        postId: "_post",
+        pageId: "_page",
+    });
+    const comment = post.addComment("fantastic");
+    expect(comment).toBeInstanceOf(Comment);
+    expect(comment.postId).toBe(post.postId);
 });
