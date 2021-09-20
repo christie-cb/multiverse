8cb69bbf1648c969ead63b0d21b84d9187862f2a
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-20 13:38:49 +0100

    Added classes to pass tests for poll

---
+++ oop-assignment/poll.js
@@ -0,0 +1,23 @@
+class Poll {
+    constructor({ question, options }) {
+        this.question = question;
+        this.options = options;
+        this.votes = { A: 0, B: 0, C: 0, D: 0 };
+    }
+
+    vote(optionLetter) {
+        this.votes[optionLetter]++;
+    }
+}
+
+class Options {
+    // Users can add at least 2 and at most 4 options.
+    constructor(A, B, C = null, D = null) {
+        this.A = A;
+        this.B = B;
+        this.C = C;
+        this.D = D;
+    }
+}
+
+module.exports = { Poll, Options };
