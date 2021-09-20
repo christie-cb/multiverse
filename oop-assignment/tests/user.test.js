de1796f755c5437f2c9771a406f6205690c5a5f9
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-20 10:20:27 +0100

    Added in forgotten new keyword:

--- oop-assignment/tests/user.test.js
+++ oop-assignment/tests/user.test.js
@@ -3,7 +3,7 @@ const User = require("../user");
 test("User includes username and user id", () => {
     const username = "username";
     const testId = "_user";
-    const user = User({ username: username, userId: testId });
+    const user = new User({ username: username, userId: testId });
     expect(user.username).toBe(username);
     expect(user.userId).toBe(testId);
 });
