5efcefd6674eaf3d2f30ee365d1d3dd5e3b9c408
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-20 13:31:21 +0100

    Added forgotten new keyword.

--- oop-assignment/tests/poll.test.js
+++ oop-assignment/tests/poll.test.js
@@ -1,12 +1,12 @@
 const { Poll, Options } = require("../poll");
 test("poll includes options", () => {
-    const options = Options({A: "scone", B: "definitely scone"});
+    const options = new Options({A: "scone", B: "definitely scone"});
     const poll = new Poll({ question: "Scone or scone?", options: options });
     expect(poll.options).toBe(options);
 });
 
 test("can vote on poll", () => {
-    const options = Options({A: "scone", B: "definitely scone"});
+    const options = new Options({A: "scone", B: "definitely scone"});
     const poll = new Poll({ question: "Scone or scone?", options: options });
     expect(poll.votes.A).toBe(0);
     expect(poll.votes.B).toBe(0);
