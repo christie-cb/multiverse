462c5856f0442ee0eea1d8f69d5e13feb95b0ee9
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-21 06:45:21 +0100

    Removing ability of user to add a post.

--- oop-assignment/tests/user.test.js
+++ oop-assignment/tests/user.test.js
@@ -8,12 +8,3 @@ test("User includes username and user id", () => {
     expect(user.username).toBe(username);
     expect(user.userId).toBe(testId);
 });
-
-test("User includes username and user id", () => {
-    const username = "username";
-    const testId = "_user";
-    const user = new User({ username: username, userId: testId });
-    const post = user.createPost({ title: "title", text: "txt", pageId: "id" });
-    expect(post).toBeInstanceOf(Post);
-    expect(post.author).toBe(user);
-});
