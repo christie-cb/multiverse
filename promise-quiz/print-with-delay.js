0c5b5160aa98d680d2f8e54e6bddc795efd332dc
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-21 14:32:40 +0100

    comparing it with .then

--- promise-quiz/print-with-delay.js
+++ promise-quiz/print-with-delay.js
@@ -15,6 +15,19 @@ async function printWithDelay(arr, delay) {
     }
 }
 
+async function printWithDelayUsingThen(arr, delay) {
+    for (let i in arr) {
+        await pause(delay).then(() => console.log(arr[i]));
+    }
+}
+
+//const arr = ["a", "b", "c"];
+//const delay = 500;
+//
+//for (let i in arr) {
+//    pause(delay).then(() => console.log(arr[i]));
+//}
+
 console.log("Are we....");
 printWithDelay([1, 2, 3], 1000);
 console.log("Blocking?");
