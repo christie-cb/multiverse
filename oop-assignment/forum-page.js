978f99c484578e4d33e57885fb492cda6c288025
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-21 06:48:17 +0100

    wrote add post method to pass the test

--- oop-assignment/forum-page.js
+++ oop-assignment/forum-page.js
@@ -2,6 +2,11 @@ class ForumPage {
     constructor({ title, pageId }) {
         this.title = title;
         this.pageId = pageId;
+        this.posts = [];
+    }
+
+    addPost(post) {
+        this.posts.push(post);
     }
 }
 
