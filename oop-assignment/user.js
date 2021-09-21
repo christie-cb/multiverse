ada79abdb39c8ee3a86b83fe1f89526948ade021
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-21 06:46:44 +0100

    Removed createPost from user class

--- oop-assignment/user.js
+++ oop-assignment/user.js
@@ -1,21 +1,9 @@
 const Post = require("./post");
-const { v4: uuidv4 } = require("uuid");
 
 class User {
     constructor({ username, userId }) {
         this.username = username;
         this.userId = userId;
-    }
-
-    createPost({ title, text, pageId }) {
-        return new Post({
-            title: title,
-            text: text,
-            date: Date(Date.now()),
-            author: this,
-            pageId: pageId,
-            postId: uuidv4(),
-        });
     }
 }
 
