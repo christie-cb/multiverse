a5d9b26f37c1626b9868776b78bff3e5690975c4
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-21 06:44:57 +0100

    Added test for adding post to forum.

--- oop-assignment/tests/forum-page.test.js
+++ oop-assignment/tests/forum-page.test.js
@@ -1,4 +1,7 @@
 const ForumPage = require("../forum-page");
+const User = require("../user");
+const Post = require("../post");
+const getTestPost = require("./utils");
 
 test("forum page contains title and page id", () => {
     const titleString = "test title";
@@ -6,4 +9,16 @@ test("forum page contains title and page id", () => {
     const forumPage = new ForumPage({ title: titleString, pageId: idString });
     expect(forumPage.title).toBe(titleString);
     expect(forumPage.pageId).toBe(idString);
+});
+
+test("forum page can add post", () => {
+    const username = "username";
+    const testId = "_user";
+    const user = new User({ username: username, userId: testId });
+    const forumPage = new ForumPage({ title: "test title", pageId: "_test" });
+    const post = getTestPost();
+    expect(forumPage.posts.length).toBe(0);
+    forumPage.addPost(post);
+    expect(forumPage.posts.length).toBe(1);
+    expect(forumPage.posts[0]).toBe(post);
 });
