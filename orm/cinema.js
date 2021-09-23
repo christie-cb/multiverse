44a8303e7b52af405e0537b48ca8a70e202eb1ec
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-23 10:39:48 +0100

    creating cinema model

---
+++ orm/cinema.js
@@ -0,0 +1,18 @@
+const sequelize = require("./db");
+const { DataTypes, Model } = require("sequelize");
+
+class Cinema extends Model {}
+
+Cinema.init(
+    {
+        location: DataTypes.STRING,
+        screenCount: DataTypes.INTEGER,
+    },
+    {
+        sequelize,
+        modelName: "Cinema",
+        timestamps: false,
+    }
+);
+
+module.exports = Cinema;
