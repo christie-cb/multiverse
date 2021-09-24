b6a59ef479a4d122290fa8a6ece898cdaf990397
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-24 10:16:22 +0100

    deleting comments

--- orm/setUp.js
+++ orm/setUp.js
@@ -4,14 +4,11 @@ const Screening = require("./models/screening");
 const db = require("./db");
 
 async function setupDb() {
-    // One cinema has many screenings.
     Cinema.hasMany(Screening);
-    // One movie has many screenings.
     Movie.hasMany(Screening);
 
-    // One-to-One relationship between Screenings and Cinemas.
-    Screening.belongsTo(Cinema); //, { foreignKey: "cinema_id" });
-    Screening.belongsTo(Movie); //, { foreignKey: "movie_id" });
+    Screening.belongsTo(Cinema);
+    Screening.belongsTo(Movie);
 
     await db.sync({ force: true, logging: console.log }); // Resets/deletes the db every time.
 }
