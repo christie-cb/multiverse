c009836ec3fc28f1b4cfc128aa1f18cd8c883ab2
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-24 14:00:48 +0100

    put request for new user

--- express/users.js
+++ express/users.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const { v4: uuidv4 } = require('uuid');
+
+
 const path = require("path");
 
 const app = express();
+app.use(express.json());
+
 const users = {
     uid1: { name: "daisy", age: 54 },
     uid2: { name: "jo", age: 34 },
@@ -11,8 +16,12 @@ app.get("/users/:userId", (req, res) => {
     res.send(users[req.params.userId]);
 });
 
-app.get("/", (req, res) => {
-    res.sendFile("index.html");
+app.post("/users", (req, res) => {
+    console.log(req.body);
+    const uid = uuidv4();
+    users[uid] = req.body;
+    console.log(users);
+    res.send(req.body);
 });
 
 const port = 3000;
