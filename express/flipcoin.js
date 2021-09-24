1ef700b66fcea0a71ddaa1edac727e9993fc9b61
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-24 14:23:02 +0100

    removing redundant stuff

--- express/flipcoin.js
+++ express/flipcoin.js
@@ -1,14 +1,7 @@
 const express = require("express");
-const path = require("path");
 
 const app = express();
 const port = 3000;
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get("/", (req, res) => {
-    res.sendFile('index.html');
-});
 
 app.get("/flipcoin", (req, res) => {
     const randomInt = Math.floor(Math.random() * 10);
