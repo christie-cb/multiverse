3bba1c88d36ec7c5c3c8b03f3f76ebd05e44142a
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-24 14:45:20 +0100

    started sudoku puzzle

---
+++ express/sudoku.js
@@ -0,0 +1,49 @@
+//const express = require("express");
+//
+//const app = express();
+//app.use(express.json());
+
+function sudoku(puzzleJson) {
+    // Example uncompleted Sudoku:
+    // {column: {row: value}}
+    // {"A": {1: 5, 3: 2}}
+    const allColumns = getEmptyAllColumns();
+    const allRows = getEmptyAllRows();
+   // Ok so I'm thinking
+    // We go thru every column and see what's inside. Push to an array.
+    // Go thru every row and do the same.
+    // Now, while loop through their solutions. We cross check the rows & columns. We can use, like a count of solutions to see whether it's filled.
+    const enteredColumns = Object.keys(puzzleJson);
+    enteredColumns.forEach((colKey) => {
+        console.log(colKey);
+        const entries = Object.values(puzzleJson[colKey]);
+        allColumns[colKey].push(entries);
+    });
+    console.log(enteredColumns);
+    console.log(allColumns);
+}
+
+function getEmptyAllRows() {
+    const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const allRows = nums.reduce(function (obj, x) {
+        obj[x] = [];
+        return obj;
+    }, {});
+    return allRows 
+}
+
+function getEmptyAllColumns() {
+    return {
+        A: [],
+        B: [],
+        C: [],
+        D: [],
+        E: [],
+        F: [],
+        G: [],
+        H: [],
+        I: [],
+    };
+}
+
+sudoku({A: {1: 5, 3: 4}});
