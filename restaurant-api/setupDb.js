fcf1c2b76550d0753756a948140310dfeae7123d
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-27 11:33:18 +0100

    added foreign key to menu to pass the test

--- restaurant-api/setupDb.js
+++ restaurant-api/setupDb.js
@@ -1,11 +1,15 @@
 const Company = require("./models/company");
+const Menu = require("./models/menu");
 const Location = require("./models/location");
 const db = require("./db");
 
 async function setupDb() {
     // Locations and menus both belong to a particular company.
     Company.hasMany(Location);
+    Company.hasMany(Menu);
+    
     Location.belongsTo(Company, { foreignKey: "CompanyId" });
+    Menu.belongsTo(Company, { foreignKey: "CompanyId" });
     await db.sync({ force: true, logging: console.log });
 }
 
