cac82ccaccee013904663aed93f80f71cbf325fa
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-27 11:00:12 +0100

    started writing for company model

---
+++ restaurant-api/tests/company.test.js
@@ -0,0 +1,16 @@
+const Company = require("../models/company");
+const setupDb = require("../setupDb");
+
+beforeAll(async () => {
+    await setupDb();
+});
+
+test("company has a name and an imageUrl", async () => {
+    const neroName = "Caffe Nero";
+    const nero = await Company.create({
+        name: neroName,
+        imageUrl: "https://picsum.photos/nero",
+    });
+    expect(nero).toBeInstanceOf(Company);
+    expect(nero.name).toBe(neroName);
+});
