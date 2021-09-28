72afe768fd8792452cf5f36f9714d7f6ae6043fb
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-28 10:12:44 +0100

    setting different db for test

--- restaurant-api/db.js
+++ restaurant-api/db.js
@@ -1,7 +1,10 @@
 const { Sequelize } = require("sequelize");
 const path = require("path");
 
-const dbPath = path.join(__dirname, "restaurant_db.sqlite");
+const dbPath =
+    process.env.NODE_ENV === "test"
+        ? "test_db.sqlite"
+        : path.join(__dirname, "restaurant_db.sqlite");
 const sequelize = new Sequelize({
     dialect: "sqlite",
     storage: dbPath,
