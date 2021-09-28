04b0d4d8b147d529c6191755bc35ecdd5f43a4f4
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-28 08:22:44 +0100

    added more failing tests for menu requests

--- restaurant-api/tests/server.test.js
+++ restaurant-api/tests/server.test.js
@@ -3,9 +3,7 @@ const request = require("supertest");
 
 describe("GET requests", () => {
     test("get all companies", function (done) {
-        request(app)
-            .get("/companies")
-            .expect(200, done);
+        request(app).get("/companies").expect(200, done);
     });
 
     test("get company by ID", async () => {
@@ -13,8 +11,19 @@ describe("GET requests", () => {
         expect(response.statusCode).toBe(200);
     });
 
-    test("get company menus by ID", async () => {
+    test("get company menus by company ID", async () => {
         const response = await request(app).get("/companies/1/menus");
+        expect(response.statusCode).toBe(200);
+    });
+    
+    test("get all menus", async () => {
+        const response = await request(app).get("/menus");
+        expect(response.statusCode).toBe(200);
+        expect(response.body).not.toBe(undefined);
+    });
+
+    test("get menu by menu ID", async () => {
+        const response = await request(app).get("/menus/1");
         expect(response.statusCode).toBe(200);
     });
 });
@@ -36,6 +45,24 @@ describe("POST requests", () => {
             })
             .catch((err) => done(err));
     });
+
+    test("create new menu", function (done) {
+        const sentData = { title: "bread" };
+        request(app)
+            .post("/menus")
+            .send(sentData)
+            .expect(200)
+            .then((response) => {
+                expect(response.body.title).toBe(sentData.title);
+                // teardown
+                const createdMenuId = response.body.id;
+                request(app)
+                    .delete(`/menus/${createdMenuId}`)
+                    .expect(200, done);
+            })
+            .catch((err) => done(err));
+    });
+
 });
 
 describe("DELETE requests", () => {
@@ -45,9 +72,26 @@ describe("DELETE requests", () => {
             .post("/companies")
             .then((response) => {
                 const companyId = response.body.id;
+                console.log(companyId);
                 // test
                 request(app)
                     .delete(`/companies/${companyId}`)
+                    .expect(200, done);
+            })
+            .catch((err) => done(err));
+    });
+
+    test("delete created menu", function (done) {
+        // setup
+        const sentData = { title: "bread" };
+        request(app)
+            .post("/menus")
+            .send(sentData)
+            .then((response) => {
+                // test
+                const createdMenuId = response.body.id;
+                request(app)
+                    .delete(`/menus/${createdMenuId}`)
                     .expect(200, done);
             })
             .catch((err) => done(err));
