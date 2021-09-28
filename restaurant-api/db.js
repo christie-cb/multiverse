998f37d538d6cbe2253a0606b1e16995b80baed9
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-28 16:24:55 +0100

    writing to memory instead of db file

--- restaurant-api/db.js
+++ restaurant-api/db.js
@@ -3,11 +3,12 @@ const path = require("path");
 
 const dbPath =
     process.env.NODE_ENV === "test"
-        ? ":memory:" //path.join(__dirname, "test_db.sqlite")
+        ? ":memory:"//path.join(__dirname, "test_db.sqlite")
         : path.join(__dirname, "restaurant_db.sqlite");
 const sequelize = new Sequelize({
     dialect: "sqlite",
     storage: dbPath,
+    logging: false,
 });
 
 module.exports = sequelize;
