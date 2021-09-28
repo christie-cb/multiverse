5b431af0e57a55c2863255b46372cbfe09789f6b
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-09-28 10:41:27 +0100

    added absolute path for test db.

--- restaurant-api/db.js
+++ restaurant-api/db.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const dbPath =
     process.env.NODE_ENV === "test"
-        ? "test_db.sqlite"
+        ? path.join(__dirname, "test_db.sqlite")
         : path.join(__dirname, "restaurant_db.sqlite");
 const sequelize = new Sequelize({
     dialect: "sqlite",
